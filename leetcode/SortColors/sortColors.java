35b15ff2069b2d0b2d4c81c6eb900ad95137e654
Daisy Gao <aringao@gmail.com>
2013-06-27 10:34:10 +0800

    Sort Colors

--- leetcode/SortColors/sortColors.java
+++ leetcode/SortColors/sortColors.java
@@ -2,19 +2,19 @@ public class Solution {
     public void sortColors(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int len = A.length, i = -1, j = 0, k = len, tmp;
-        while (j < k) {
-            if (A[j] == 0) {
-                tmp = A[++i];
-                A[i] = A[j];
-                A[j] = tmp;
-                j++;
-            } else if (A[j] == 2) {
-                tmp = A[--k];
-                A[k] = A[j];
-                A[j] = tmp;
+        int len = A.length, i = -1, j = len, k = 0, x;
+        while (k < j) {
+            if (A[k] == 2) {
+                x = A[k];
+                A[k] = A[--j];
+                A[j] = x;
             } else {
-                j++;
+                if (A[k] == 0) {
+                    x = A[k];
+                    A[k] = A[++i];
+                    A[i] = x;
+                }
+                k++;
             }
         }
     }
