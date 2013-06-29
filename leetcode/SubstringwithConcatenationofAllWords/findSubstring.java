97d81a648dc1b328e6431f26ff1edede25591838
Daisy Gao <aringao@gmail.com>
2013-06-29 17:33:49 +0800

    Substring with Concatenation of All Words

--- leetcode/SubstringwithConcatenationofAllWords/findSubstring.java
+++ leetcode/SubstringwithConcatenationofAllWords/findSubstring.java
@@ -2,38 +2,29 @@ public class Solution {
     public ArrayList<Integer> findSubstring(String S, String[] L) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        ArrayList<Integer> ans = new ArrayList<Integer>();
         int slen = S.length(), llen = L.length;
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        if (slen == 0 || llen == 0) return result;
-        HashMap<String, Integer> cntMap = new HashMap<String, Integer>();
-        HashMap<String, Integer> curCnt = new HashMap<String, Integer>();
-        for (String l : L) {
-            int cnt = cntMap.containsKey(l) ? cntMap.get(l) : 0;
-            cntMap.put(l, cnt + 1);
-        }
+        if (slen == 0 || llen == 0) return ans;
         int wlen = L[0].length();
-        for (int i = 0; i <= slen - llen * wlen; i++) {
-            curCnt.clear();
-            boolean flag = true;
-            for (int j = 0; j < llen; j++) {
-                String str = S.substring(i + j * wlen, i + (j + 1) * wlen);
-                if (cntMap.containsKey(str)) {
-                    int cnt = curCnt.containsKey(str) ? curCnt.get(str) : 0;
-                    if (cnt < cntMap.get(str)) {
-                        curCnt.put(str, cnt + 1);
-                    } else {
-                        flag = false;
-                        break;
-                    }
-                } else {
-                    flag = false;
-                    break;
-                }
-            } 
-            if (flag) {
-                result.add(i);
-            }
+        HashMap<String, Integer> needed = new HashMap<String, Integer>();
+        for (String word : L) {
+            int cnt = needed.containsKey(word) ? needed.get(word) : 0;
+            needed.put(word, cnt + 1);
         }
-        return result;
+        for (int i = 0; i <= slen - llen * wlen; i++) {
+            int j = i, total = 0;
+            HashMap<String, Integer> found = new HashMap<String, Integer>();
+            while (j + wlen <= slen && total < llen) {
+                String word = S.substring(j, j + wlen);
+                int cnt = found.containsKey(word) ? found.get(word) : 0;
+                if (needed.containsKey(word) && cnt < needed.get(word)) {
+                    found.put(word, cnt + 1);
+                    j += wlen;
+                    total++;
+                } else break;
+            }
+            if (total == llen) ans.add(i);
+        }
+        return ans;
     }
 }
