dfc2a8aa048bddec86e8dcbc41cc8560e3f45312
Daisy Gao <aringao@gmail.com>
2013-07-02 09:21:32 +0800

    Trapping Rain Water

--- leetcode/TrappingRainWater/trap.java
+++ leetcode/TrappingRainWater/trap.java
@@ -2,18 +2,14 @@ public class Solution {
     public int trap(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int len = A.length;
-        if (len < 3) return 0;
-        int maxArr[] = new int[len];
+        int len = A.length, left[] = new int[len], ans = 0, right = Integer.MIN_VALUE;
         for (int i = 0; i < len; i++) {
-            maxArr[i] = i == 0 ? A[i] : (A[i] > maxArr[i - 1] ? A[i] : maxArr[i - 1]);
+            left[i] = Math.max(A[i], i == 0 ? 0 : left[i - 1]);
         }
-        int result = 0;
         for (int i = len - 1; i >= 0; i--) {
-            int tmp = i == len - 1 ? A[i] : (A[i] > maxArr[i + 1] ? A[i] : maxArr[i + 1]);
-            maxArr[i] = Math.min(tmp, maxArr[i]);
-            result += maxArr[i] - A[i];
+            right = Math.max(right, A[i]);
+            ans += Math.min(left[i], right) - A[i];
         }
-        return result;
+        return ans;
     }
 }
