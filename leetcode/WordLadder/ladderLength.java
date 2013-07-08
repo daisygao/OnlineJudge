ea8707d394958a1134898415449169c2c51bb26b
Daisy Gao <aringao@gmail.com>
2013-07-08 14:19:00 +0800

    Modify Word Ladder return condition

--- leetcode/WordLadder/ladderLength.java
+++ leetcode/WordLadder/ladderLength.java
@@ -1,4 +1,5 @@
 public class Solution {
+
     public int ladderLength(String start, String end, HashSet<String> dict) {
         // Start typing your Java solution below
         // DO NOT write main() function
@@ -9,7 +10,7 @@ public class Solution {
         path.put(start, 1);
         while (idx < queue.size()) {
             String u = queue.get(idx);
-            for (int i = 0; i < u.length(); i++) { 
+            for (int i = 0; i < u.length(); i++) {
                 for (char c = 'a'; c <= 'z'; c++) {
                     if (c == u.charAt(i)) continue;
                     StringBuilder sb = new StringBuilder(u);
@@ -19,11 +20,11 @@ public class Solution {
                         path.put(v, path.get(u) + 1);
                         queue.add(v);
                     }
-                    if (v.equals(end)) {
+                    if (v.equals(end) && (!u.equals(start) || dict.contains(start) || dict.contains(end))) {
                         return path.get(u) + 1;
                     }
                 }
-            }            
+            }           
             idx++;
         }
         return 0;
