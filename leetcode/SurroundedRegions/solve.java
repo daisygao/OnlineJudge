4f752b1a6ce43a0d377de95d6ff18884e3caf716
Daisy Gao <aringao@gmail.com>
2013-07-26 19:33:58 +0800

    Surrounded Regions

--- leetcode/SurroundedRegions/solve.java
+++ leetcode/SurroundedRegions/solve.java
@@ -1,6 +1,7 @@
 public class Solution {
     private class Point {
-        public int x, y;
+        int x;
+        int y;
         Point(int x, int y) {
             this.x = x;
             this.y = y;
@@ -12,32 +13,31 @@ public class Solution {
         int height = board.length;
         if (height == 0) return;
         int width = board[0].length;
-        int dx[] = {0, -1, 0, 1}, dy[] = {-1, 0, 1, 0};
-        ArrayList<Point> list = new ArrayList<Point>(100000);
+        ArrayList<Point> queue = new ArrayList<Point>(height * width);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (board[i][j] == 'O' && (i == 0 || j == 0 || i == height - 1 || j == width - 1)) {
+                if ((i == 0 || j == 0 || i == height -1 || j == width - 1) 
+                && board[i][j] == 'O') {
                     board[i][j] = 'D';
-                    list.add(new Point(i, j));
+                    queue.add(new Point(i, j));
                 }
             }
         }
-        int idx = 0;
-        while (idx < list.size()) {
-            Point cur = list.get(idx);
+        int idx = 0, dx[] = {0, -1, 0, 1}, dy[] = {-1, 0, 1, 0};
+        while (idx < queue.size()) {
+            int x = queue.get(idx).x, y = queue.get(idx).y;
             for (int i = 0; i < dx.length; i++) {
-                int xx = cur.x + dx[i], yy = cur.y + dy[i];
+                int xx = x + dx[i], yy = y + dy[i];
                 if (xx >= 0 && yy >= 0 && xx < height && yy < width && board[xx][yy] == 'O') {
                     board[xx][yy] = 'D';
-                    list.add(new Point(xx, yy));
+                    queue.add(new Point(xx, yy));
                 }
             }
             idx++;
         }
-    
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                board[i][j] = (board[i][j] == 'D' ? 'O' : 'X');
+                board[i][j] = board[i][j] == 'D' ? 'O' : 'X';
             }
         }
     }
