dd5ab903ee8cdcc81fc55cb5ba831102eca64438
Daisy Gao <aringao@gmail.com>
2013-07-30 15:01:56 +0800

    Longest Consecutive Sequence

--- leetcode/LongestConsecutiveSequence/longestConsecutive.java
+++ leetcode/LongestConsecutiveSequence/longestConsecutive.java
@@ -2,28 +2,21 @@ public class Solution {
     public int longestConsecutive(int[] num) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int len = num.length;
-        if (len == 0) return 0;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> range = new HashMap<Integer, Integer>();
         int max = 1;
         for (int x : num) {
-            if (map.containsKey(x)) continue;
-            map.put(x, 1);
-            if (map.containsKey(x - 1)) {
-                max = Math.max(max, merge(map, x - 1, x));
-            } 
-            if (map.containsKey(x + 1)) {
-                max = Math.max(max, merge(map, x, x + 1));
+            if (!range.containsKey(x)) {
+                range.put(x, 1);
+                if (range.containsKey(x - 1)) max = Math.max(max, merge(x - 1, x, range));
+                if (range.containsKey(x + 1)) max = Math.max(max, merge(x, x + 1, range));
             }
         }
         return max;
     }
-    public int merge(HashMap<Integer, Integer> map, int left, int right) {
-        int upper = right + map.get(right) - 1;
-        int lower = left - map.get(left) + 1;
-        int len = upper - lower + 1;
-        map.put(upper, len);
-        map.put(lower, len);
+    private int merge(int x, int y, HashMap<Integer, Integer> range) {
+        int left = x - range.get(x) + 1, right = y + range.get(y) - 1, len = right - left + 1;
+        range.put(left, len);
+        range.put(right, len);
         return len;
     }
 }
