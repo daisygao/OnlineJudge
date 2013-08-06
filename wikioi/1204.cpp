f5a49352a168f14ef67b17ba1dd52b4ace3992c5
Daisy Gao <aringao@gmail.com>
2013-08-06 09:27:02 +0800

    1204 strstr

---
+++ wikioi/1204.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s1, s2;
+    cin >> s1 >> s2;
+    int i, j;
+    for (i = 0; i < s1.length(); i++) {
+        for (j = 0; j < s2.length(); j++) {
+            if (s1[i + j] != s2[j]) break;
+        }
+        if (j == s2.length()) cout << i + 1;
+    }
+    return 0;
+}
+	
