e06f11c86d03a5b0e37a4dc122bd4cea13e6a911
Daisy Gao <aringao@gmail.com>
2013-08-08 00:40:10 +0800

    kmp

---
+++ wikioi/1204_kmp.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void calcPos(string pat, int* arr) {
+    int match = 0, idx = 1, len = pat.length();
+    arr[0] = 0;
+    while (idx != len) {        
+        if (pat[match] == pat[idx]) {
+            arr[idx++] = ++match;
+        } else if (match == 0) {
+            arr[idx++] = 0;
+        } else {
+            match = arr[match - 1];
+        }        
+    }
+}
+int main() {
+	string s, pat;
+    cin >> s >> pat;
+    int slen = s.length(), plen = pat.length(), *pos = new int[plen], sidx = 0, pidx = 0;
+    calcPos(pat, pos);
+    while (sidx < slen) {
+        if (s[sidx] == pat[pidx]) {
+            sidx++;
+            pidx++;
+        } else if (pidx == 0) {
+            sidx++;
+        } else {
+        	pidx = pos[pidx - 1];
+        }
+        if (pidx == plen) break;
+    }
+    delete [] pos;
+    cout << (pidx == plen ? (sidx - plen + 1) : 0);
+    return 0;
+}
