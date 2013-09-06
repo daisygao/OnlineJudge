92d2dd7701e8c0f71a0ec91f8f94f343efea1a85
Daisy Gao <aringao@gmail.com>
2013-09-06 08:31:48 +0800

    Word Ladder

--- leetcode/WordLadder/ladderLength.java
+++ leetcode/WordLadder/ladderLength.java
@@ -1,31 +1,30 @@
 public class Solution {
-
     public int ladderLength(String start, String end, HashSet<String> dict) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int idx = 0;
-        ArrayList<String> queue = new ArrayList<String>(dict.size());
-        HashMap<String, Integer> path = new HashMap<String, Integer>();
-        queue.add(start);
-        path.put(start, 1);
-        while (idx < queue.size()) {
-            String u = queue.get(idx);
-            for (int i = 0; i < u.length(); i++) {
+        ArrayList<String> vq = new ArrayList<String>(dict.size());
+        HashMap<String, Integer> reached = new HashMap<String, Integer>(dict.size());
+        int i = 0;
+        vq.add(start);
+        reached.put(start, 1);
+        while (i != vq.size()) {
+            String v = vq.get(i);
+            int d = reached.get(v);
+            for (int j = 0; j < v.length(); j++) {
                 for (char c = 'a'; c <= 'z'; c++) {
-                    if (c == u.charAt(i)) continue;
-                    StringBuilder sb = new StringBuilder(u);
-                    sb.setCharAt(i, c);
-                    String v = sb.toString();
-                    if (dict.contains(v) && !path.containsKey(v)) {
-                        path.put(v, path.get(u) + 1);
-                        queue.add(v);
+                    if (c == v.charAt(j)) continue;
+                    StringBuilder sb = new StringBuilder(v);
+                    sb.setCharAt(j, c);
+                    String u = sb.toString();
+                    if (u.equals(end) && (d != 1 || dict.contains(start) || dict.contains(end))) {
+                        return d + 1;
                     }
-                    if (v.equals(end) && (!u.equals(start) || dict.contains(start) || dict.contains(end))) {
-                        return path.get(u) + 1;
-                    }
+                    if (!dict.contains(u) || reached.containsKey(u)) continue;
+                    vq.add(u);
+                    reached.put(u, d + 1);
                 }
-            }           
-            idx++;
+            }
+            i++;
         }
         return 0;
     }
