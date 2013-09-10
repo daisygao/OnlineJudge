e72f6900cfdbaa70e8a0b49cc70b4ab15f4614a4
Daisy Gao <aringao@gmail.com>
2013-09-10 19:17:46 +0800

    Best Time to Buy and Sell Stock II

--- leetcode/BestTimetoBuyandSellStockII/maxProfit.java
+++ leetcode/BestTimetoBuyandSellStockII/maxProfit.java
@@ -2,15 +2,13 @@ public class Solution {
     public int maxProfit(int[] prices) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int len = prices.length, idx = 0, min, max, profit = 0;
-        while (idx < len) {
-            while (idx + 1 < len && prices[idx + 1] <= prices[idx]) idx++;
-            min = prices[idx];
-            while (idx + 1 < len && prices[idx + 1] >= prices[idx]) idx++;
-            max = prices[idx];
-            if (max == min) break;
-            profit += max - min;
+        int len = prices.length, i = 0, j = 0, ans = 0;
+        while (j < len) {
+            while (j + 1 < len && prices[j + 1] <= prices[j]) j++;
+            i = j;
+            while (j + 1 < len && prices[j + 1] >=prices[j]) j++;
+            ans += prices[j++] - prices[i];
         }
-        return profit;
+        return ans;
     }
 }
