a7a4665cf4052085a4e5e8d8877382a359872c31
Daisy Gao <aringao@gmail.com>
2013-09-11 22:15:36 +0800

    Triangle

--- leetcode/Triangle/minimumTotal.java
+++ leetcode/Triangle/minimumTotal.java
@@ -2,17 +2,13 @@ public class Solution {
     public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int n = triangle.size(), ans = 0, sols[] = new int[n];
-        for (ArrayList<Integer> list : triangle) {
-            ans = Integer.MAX_VALUE;
-            for (int i = list.size() - 1; i >= 0; i--) {
-                int prev = 0;
-                if (i == list.size() -1 && i == 0) prev = 0;
-                else if (i == list.size() - 1) prev = sols[i - 1];
-                else if (i == 0) prev = sols[i];
-                else prev = Math.min(sols[i - 1], sols[i]);
-                sols[i] = list.get(i) + prev;
-                ans = Math.min(sols[i], ans);
+        int len = triangle.size(), sols[] = new int[len], ans = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j >= 0; j--) {
+                sols[j] = triangle.get(i).get(j) + (i == 0 ? 0 : Math.min(
+                    j == 0 ? Integer.MAX_VALUE : sols[j - 1],
+                    j == i ? Integer.MAX_VALUE : sols[j]));
+                if (i == len - 1 && sols[j] < ans) ans = sols[j];
             }
         }
         return ans;
