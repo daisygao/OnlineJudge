00e0bbf7169b7a0a9d19ce28040cc52df41959e7
Daisy Gao <aringao@gmail.com>
2013-09-13 00:25:26 +0800

    Pascal's Triangle II

--- leetcode/PascalsTriangleII/getRow.java
+++ leetcode/PascalsTriangleII/getRow.java
@@ -2,17 +2,17 @@ public class Solution {
     public ArrayList<Integer> getRow(int rowIndex) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        ArrayList<Integer> row = new ArrayList<Integer>();
-        int idx = 0;
-        row.add(1);
-        while (++idx <= rowIndex) {
-            for (int i = idx; i >= 0; i--) {
-                int x = (i - 1 >= 0 ? row.get(i - 1) : 0) 
-		    + (i == idx ? 0 : row.get(i));
-                if (i == idx) row.add(x);
-                else row.set(i, x);
+        ArrayList<Integer> ans = new ArrayList<Integer>(rowIndex + 1);
+        for (int i = 0; i <= rowIndex; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (j == i) {
+                    int x = i == 0 ? 1 : ans.get(ans.size() - 1);
+                    ans.add(x);
+                } else {
+                    ans.set(j, (j == 0 ? 0 : ans.get(j - 1)) + ans.get(j));
+                }
             }
         }
-        return row;
+        return ans;
     }
 }
