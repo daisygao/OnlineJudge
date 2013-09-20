934ed5e6629ca5d882f4e4f3973d06d5b8dba1a5
Daisy Gao <aringao@gmail.com>
2013-09-20 21:07:48 +0800

    Distinct Subsequences

--- leetcode/DistinctSubsequences/numDistinct_2DArray.java
+++ leetcode/DistinctSubsequences/numDistinct_2DArray.java
@@ -2,16 +2,14 @@ public class Solution {
     public int numDistinct(String S, String T) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int slen = S.length(), tlen = T.length();
-        if (tlen == 0 || slen < tlen) return 0;
-        int sol[][] = new int[tlen][slen];
-        for (int i = 0; i < tlen; i++) {
-            for (int j = i; j < slen; j++) {
-                sol[i][j] = (j == 0 ? 0 : sol[i][j - 1])
-		    + (T.charAt(i) == S.charAt(j) ?
-		       (i == 0 || j == 0 ? 1 : sol[i - 1][j - 1]) : 0);
+        int n = S.length(), m = T.length();
+        if (n < m) return 0;
+        int sols[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < n; j++) {
+                sols[i][j] = (j == 0 ? 0 : sols[i][j - 1]) + (S.charAt(j) == T.charAt(i) ? (i == 0 ? 1 : sols[i - 1][j - 1]) : 0);
             }
         }
-        return sol[tlen - 1][slen - 1];
+        return sols[m - 1][n - 1];
     }
 }
