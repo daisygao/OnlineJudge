4eba5fe431484a8d940b425747340633a8417bc1
Daisy Gao <aringao@gmail.com>
2013-09-20 18:03:38 +0800

    Median of Two Sorted Arrays

--- leetcode/MedianOfTwoSortedArrays/findMedianSortedArrays.java
+++ leetcode/MedianOfTwoSortedArrays/findMedianSortedArrays.java
@@ -2,44 +2,28 @@ public class Solution {
     public double findMedianSortedArrays(int A[], int B[]) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int alen = A.length, blen = B.length, len = alen + blen, needed = (len - 1) / 2;
-        int ahead = 0, atail = Math.min(alen, needed) - 1, bhead = 0, btail = Math.min(blen, needed) - 1, amid, bmid;
-        while (true) {
-            
-            if (ahead > atail) {
-                bhead = needed - ahead;
-            } else if (bhead > btail) {
-                ahead = needed - bhead;
-            }
-            
-            if (ahead + bhead == needed) {
-                int a0 = ahead >= alen ? Integer.MAX_VALUE : A[ahead];
-                int b0 = bhead >= blen ? Integer.MAX_VALUE : B[bhead];
-                int first = Math.min(a0, b0), second = Math.max(a0, b0);
-                int a1 = ahead + 1 >= alen ? Integer.MAX_VALUE : A[ahead + 1];
-                int b1 = bhead + 1 >= blen ? Integer.MAX_VALUE : B[bhead + 1];
-                second = Math.min(second, a1);
-                second = Math.min(second, b1);
-                return len % 2 == 0 ? (first + second) / 2.0 : first;
-            } 
-            
-            amid = ahead + (atail - ahead) / 2;
-            bmid = bhead + (btail - bhead) / 2;
-            if (amid + bmid < needed) {
-                if (A[amid] <= B[bmid]) {
-                    ahead = amid + 1;
-                    //btail = Math.min(btail, needed - ahead);
+        int sum = A.length + B.length;
+        return sum % 2 == 0 ? (helper(A, B, sum / 2 - 1) + helper(A, B, sum / 2)) / 2.0 : helper(A, B, sum / 2);
+    }
+    private double helper(int A[], int B[], int k) {
+        int alen = A.length, blen = B.length, al = 0, bl = 0, ar = alen - 1, br = blen - 1, am, bm;
+        while (al <= ar || bl <= br) {
+            if (al <= ar && bl <= br) {
+                am = al + (ar - al >> 1);
+                bm = bl + (br - bl >> 1);
+                if (A[am] >= B[bm]) {
+                    if (k > bm && am + bm + 1 <= k) return helper(A, Arrays.copyOfRange(B, bm + 1, blen), k - bm - 1);
+                    else return helper(Arrays.copyOfRange(A, 0, am), B, k);
                 } else {
-                    bhead = bmid + 1;
-                    //atail = Math.min(atail, needed - bhead);
-                }
+                    if (k > am && am + bm + 1 <= k) return helper(Arrays.copyOfRange(A, am + 1, alen), B, k - am - 1);
+                    else return helper(A, Arrays.copyOfRange(B, 0, bm), k);
+                } 
+            } else if (al <= ar) {
+                if (k < alen) return A[k];
             } else {
-                if (A[amid] <= B[bmid]) {
-                    btail = bmid - 1;
-                } else {
-                    atail = amid - 1;
-                }
+                if (k < blen) return B[k];
             }
         }
+        return 0;
     }
 }
