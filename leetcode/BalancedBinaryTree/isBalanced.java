fa2bb8c60e7b88b54a6409f79f55d5ccb7b1bde8
Daisy Gao <aringao@gmail.com>
2013-09-21 15:05:52 +0800

    Balanced Binary Tree

--- leetcode/BalancedBinaryTree/isBalanced.java
+++ leetcode/BalancedBinaryTree/isBalanced.java
@@ -8,11 +8,15 @@
  * }
  */
 public class Solution {
-    public int minDepth(TreeNode root) {
+    public boolean isBalanced(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        return helper(root) != -1;
+    }
+    private int helper(TreeNode root) {
         if (root == null) return 0;
-        int left = minDepth(root.left), right = minDepth(root.right);
-        return 1 + (root.left != null && root.right != null ? Math.min(left, right) : Math.max(left, right));
+        int left = helper(root.left), right = helper(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
     }
 }
