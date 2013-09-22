81fecffd1df8a4f95a65f2300e2f1550928cefde
Daisy Gao <aringao@gmail.com>
2013-09-22 18:10:59 +0800

    Construct Binary Tree from Inorder and Postorder Traversal

--- leetcode/ConstructBinaryTreefromInorderandPostorderTraversal/buildTree.java
+++ leetcode/ConstructBinaryTreefromInorderandPostorderTraversal/buildTree.java
@@ -11,20 +11,18 @@ public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int len = postorder.length;
+        int len = inorder.length;
         if (len == 0) return null;
-        int rootVal = postorder[len - 1];
-        int left = 0;
-        for (left = 0; left < len; left++) {
-            if (inorder[left] == rootVal) {
-                break;
-            }
-        }
-        TreeNode root = new TreeNode(rootVal);
-        root.left = buildTree(Arrays.copyOfRange(inorder, 0, left),
-			      Arrays.copyOfRange(postorder, 0, left));
-        root.right = buildTree(Arrays.copyOfRange(inorder, left + 1, len),
-			       Arrays.copyOfRange(postorder, left, len - 1));
+        int x = postorder[len - 1], mid = indexOf(inorder, x);
+        TreeNode root = new TreeNode(x);
+        root.left = buildTree(Arrays.copyOfRange(inorder, 0, mid), Arrays.copyOfRange(postorder, 0, mid));
+        root.right = buildTree(Arrays.copyOfRange(inorder, mid + 1, len), Arrays.copyOfRange(postorder, mid, len - 1));
         return root;
+    }
+    private int indexOf(int arr[], int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) return i;
+        }
+        return -1;
     }
 }
