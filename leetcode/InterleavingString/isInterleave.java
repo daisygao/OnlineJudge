741df564032a3fd62a58802d8022fe3a9eadc131
Daisy Gao <aringao@gmail.com>
2013-09-24 19:10:55 +0800

    Interleaving String

--- leetcode/InterleavingString/isInterleave.java
+++ leetcode/InterleavingString/isInterleave.java
@@ -4,19 +4,18 @@ public class Solution {
         // DO NOT write main() function
         int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
         if (l1 + l2 != l3) return false;
-        boolean sol[][] = new boolean[l1 + 1][l2 + 1];
+        boolean sols[][] = new boolean[l1 + 1][l2 + 1];
         for (int i = 0; i <= l1; i++) {
             for (int j = 0; j <= l2; j++) {
-                int k = i + j - 1;
-                if (i > 0 && s3.charAt(k) == s1.charAt(i - 1)) {
-                    sol[i][j] |= sol[i - 1][j];
+                if (i == 0 && j == 0) sols[i][j] = true;
+                if (i > 0 && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    sols[i][j] |= sols[i - 1][j];
                 }
-                if (j > 0 && s3.charAt(k) == s2.charAt(j - 1)) {
-                    sol[i][j] |= sol[i][j - 1];
+                if (j > 0 && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    sols[i][j] |= sols[i][j - 1];
                 }
-                if (i == 0 && j == 0) sol[i][j] = true;
             }
         }
-        return sol[l1][l2];
+        return sols[l1][l2];
     }
 }
