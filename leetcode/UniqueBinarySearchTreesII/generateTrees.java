eb2bc2d238bcc320ee2ae1bae5c0c27fcc88f75c
Daisy Gao <aringao@gmail.com>
2013-09-24 23:44:19 +0800

    Unique Binary Search Trees II

--- leetcode/UniqueBinarySearchTreesII/generateTrees.java
+++ leetcode/UniqueBinarySearchTreesII/generateTrees.java
@@ -1,36 +1,32 @@
-
 /**
-* Definition for binary tree
-* public class TreeNode {
-*     int val;
-*     TreeNode left;
-*     TreeNode right;
-*     TreeNode(int x) { val = x; left = null; right = null; }
-* }
-*/
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; left = null; right = null; }
+ * }
+ */
 public class Solution {
     public ArrayList<TreeNode> generateTrees(int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
         return helper(1, n);
     }
-    private ArrayList<TreeNode> helper(int low, int high) {
+    private ArrayList<TreeNode> helper(int start, int end) {
         ArrayList<TreeNode> ans = new ArrayList<TreeNode>();
-        if (low >= high) {            TreeNode root = low == high ? new TreeNode(low) : null;
-            ans.add(root);
-        } else {
-            for (int i = low; i <= high; i++) {
-                for (TreeNode left : helper(low, i - 1)) {
-                    for (TreeNode right : helper(i + 1, high)) {
-                        TreeNode root = new TreeNode(i);
-                        root.left = left;
-                        root.right = right;
-                        ans.add(root);
-                    }
+        if (start > end) ans.add(null);
+        for (int mid = start; mid <= end; mid++) {
+            for (TreeNode lc : helper(start, mid - 1)) {
+                for (TreeNode rc : helper(mid + 1, end)) {
+                    TreeNode root = new TreeNode(mid);
+                    root.left = lc;
+                    root.right = rc;
+                    ans.add(root);
                 }
             }
         }
-        return ans;        
+        return ans;
+        
     }
 }
-
