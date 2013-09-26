641a4ac3cb1a2d14f151cb77389a4647285cd890
Daisy Gao <aringao@gmail.com>
2013-09-26 15:10:42 +0800

    Unique Binary Search Trees

--- leetcode/UniqueBinarySearchTrees/numTrees.java
+++ leetcode/UniqueBinarySearchTrees/numTrees.java
@@ -5,9 +5,10 @@ public class Solution {
         int sols[] = new int[n + 1];
         sols[0] = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                sols[i] += sols[i - j] * sols[j - 1];
+            for (int j = 1; j <= i / 2; j++) {
+                sols[i] += 2 * sols[j - 1] * sols[i - j];
             }
+            if (i % 2 != 0) sols[i] += sols[i / 2] * sols[i / 2];
         }
         return sols[n];
     }
