c4b58729f8c8073ce3fa4fdb0289478a728c15bc
Daisy Gao <aringao@gmail.com>
2013-09-27 17:57:24 +0800

    Subsets II

--- leetcode/SubsetsII/subsetsWithDup.java
+++ leetcode/SubsetsII/subsetsWithDup.java
@@ -2,24 +2,23 @@ public class Solution {
     public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> idx = new ArrayList<Integer>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        result.add(list);
-        idx.add(-1);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>(), pos = new ArrayList<Integer>();
+        ans.add(list);
+        pos.add(0);
         Arrays.sort(num);
-        int cur = 0, start, len = num.length;
-        while (cur < result.size()) {
-            start = idx.get(cur) + 1;
-            for (; start < len; start++) {
-                list = new ArrayList<Integer>(result.get(cur));
-                list.add(num[start]);
-                result.add(list);
-                idx.add(start);
-                while (start + 1 < len && num[start + 1] == num[start]) start++;
+        int idx = 0, len = num.length;
+        while (idx < ans.size()) {
+            list = ans.get(idx);
+            for (int s = pos.get(idx); s < len; s++) {
+                ArrayList<Integer> newList = new ArrayList<Integer>(list);
+                newList.add(num[s]);
+                ans.add(newList);
+                pos.add(s + 1);
+                while (s + 1 < len && num[s + 1] == num[s]) s++;
             }
-            cur++;
+            idx++;
         }
-        return result;
+        return ans;
     }
 }
