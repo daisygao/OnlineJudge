4706a54a16909bcae4662869936d70bf919fe1bb
Daisy Gao <aringao@gmail.com>
2013-09-27 11:37:48 +0800

    Restore IP Addresses

--- leetcode/RestoreIPAddresses/restoreIpAddresses.java
+++ leetcode/RestoreIPAddresses/restoreIpAddresses.java
@@ -2,24 +2,24 @@ public class Solution {
     public ArrayList<String> restoreIpAddresses(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        return helper(s, 0); 
+        return helper(s, 1);
     }
     private ArrayList<String> helper(String s, int cnt) {
-        int slen = s.length();
-        ArrayList<String> result = new ArrayList<String>();
-        for (int len = Math.min(slen, 3); len > 0; len--) {
-            String num = s.substring(0, len);
-            int val = Integer.parseInt(num);
-            if ((num.charAt(0) != '0' || len == 1) && (val >= 0 && val < 256)) {
-                if (cnt == 3 && len == slen) {
-                    result.add(num);
-                } else if (cnt < 3) {
-                    for (String back : helper(s.substring(len), cnt + 1)) {
-                        result.add(num + "." + back);
+        ArrayList<String> ans = new ArrayList<String>();
+        int len = s.length();
+        for (int i = 1; i <= Math.min(3, len); i++) {
+            String s1 = s.substring(0, i), s2 = s.substring(i);
+            int x = Integer.valueOf(s1);
+            if (x >= 0 && x < 256 && (s1.charAt(0) != '0' || s1.length() == 1)) {
+                if (cnt == 4 && i == len) {
+                    ans.add(s);
+                } else if (cnt < 4) {
+                    for (String back : helper(s2, cnt + 1)) {
+                        ans.add(s1 + "." + back);
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 }
