5daca613a9147960cdd4402d2a175954153d3e02
Daisy Gao <aringao@gmail.com>
2013-09-28 23:39:40 +0800

    Merge Sorted Array “

--- leetcode/MergeSortedArray/merge.java
+++ leetcode/MergeSortedArray/merge.java
@@ -2,12 +2,9 @@ public class Solution {
     public void merge(int A[], int m, int B[], int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int a = m - 1, b = n - 1, c = m + n - 1;
-        while (a >= 0 && b >= 0) {
-            A[c--] = A[a] >= B[b] ? A[a--] : B[b--];
-        }
+        int a = m - 1, b = n - 1, i = m + n - 1;
         while (b >= 0) {
-            A[c--] = B[b--];
+            A[i--] = a >= 0 && A[a] > B[b] ? A[a--] : B[b--];
         }
     }
 }
