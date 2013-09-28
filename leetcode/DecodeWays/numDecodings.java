25a8a55499ba233ea3295e6ea91cde3af0e77328
Daisy Gao <aringao@gmail.com>
2013-09-28 23:07:28 +0800

    Decode Ways

--- leetcode/DecodeWays/numDecodings.java
+++ leetcode/DecodeWays/numDecodings.java
@@ -2,16 +2,14 @@ public class Solution {
     public int numDecodings(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int len = s.length();
+        int len = s.length(), sols[] = new int[len];
         if (len == 0) return 0;
-        int sol[] = new int[len];
         for (int i = 0; i < len; i++) {
-            if (s.charAt(i) != '0') sol[i] += i - 1 >= 0 ? sol[i - 1] : 1;
-            if (i - 1 >= 0 && s.charAt(i - 1) != '0' 
-		&& Integer.parseInt(s.substring(i - 1, i + 1)) < 27) {
-                sol[i] += i - 2 >= 0 ? sol[i - 2] : 1;
+            if (s.charAt(i) != '0') sols[i] += (i == 0 ? 1 : sols[i - 1]);
+            if (i > 0 && s.charAt(i - 1) != '0' && Integer.valueOf(s.substring(i - 1, i + 1)) < 27) {
+                sols[i] += (i == 1 ? 1 : sols[i - 2]);
             }
         }
-        return sol[len - 1];
+        return sols[len - 1];
     }
 }
