b71c332582172c9cf228178d012ed6a713ba0323
Daisy Gao <aringao@gmail.com>
2013-10-01 00:03:38 +0800

    Maximal Rectangle

--- leetcode/MaximalRectangle/maximalRectangle.java
+++ leetcode/MaximalRectangle/maximalRectangle.java
@@ -4,30 +4,25 @@ public class Solution {
         // DO NOT write main() function
         int height = matrix.length;
         if (height == 0) return 0;
-        int width = matrix[0].length, max = 0;
-        int col[] = new int[width], left[] = new int[width], right[] = new int[width];
+        int width = matrix[0].length, hist[] = new int[width], left[] = new int[width], ans = 0;
         Stack<Integer> st = new Stack<Integer>();
         for (int i = 0; i < height; i++) {
             st.clear();
             for (int j = 0; j < width; j++) {
-                col[j] = matrix[i][j] == '0' ? 0 :
-                    ((i - 1 >= 0 ? col[j] : 0) + 1);
-                while (!st.empty() && col[st.peek()] >= col[j]) {
-                    st.pop();
-                }
-                left[j] = st.empty() ? 0 : st.peek() + 1;
+                hist[j] = matrix[i][j] == '0' ? 0 : (hist[j] + 1);
+                while (!st.empty() && hist[st.peek()] >= hist[j]) st.pop();
+                left[j] = st.empty() ? 0 : (st.peek() + 1);
                 st.push(j);
             }
             st.clear();
+            int right = 0;
             for (int j = width - 1; j >= 0; j--) {
-                while (!st.empty() && col[st.peek()] >= col[j]) {
-                    st.pop();
-                }
-                right[j] = st.empty() ? width - 1 : st.peek() - 1;
+                while (!st.empty() && hist[st.peek()] >= hist[j]) st.pop();
+                right = st.empty() ? width : st.peek();
                 st.push(j);
-                max = Math.max(max, col[j] * (right[j] - left[j] + 1));
+                ans = Math.max(ans, hist[j] * (right - left[j]));
             }
         }
-        return max;
+        return ans;
     }
 }
