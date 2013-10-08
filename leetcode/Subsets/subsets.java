b42d623494669e41c3c84df6dcb2c387fd9c9da6
Daisy Gao <aringao@gmail.com>
2013-10-08 23:56:19 +0800

    Subsets

--- leetcode/Subsets/subsets.java
+++ leetcode/Subsets/subsets.java
@@ -1,19 +1,18 @@
+
+10.8.2013
 public class Solution {
     public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        ans.add(new ArrayList<Integer>());
         Arrays.sort(S);
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>(10000);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        result.add(list);
         for (int x : S) {
-            int size = result.size();
-            for (int i = 0; i < size; i++) {
-                list = new ArrayList<Integer>(result.get(i));
+            for (int i = ans.size() - 1; i >= 0; i--) {
+                ArrayList<Integer> list = new ArrayList<Integer>(ans.get(i));
                 list.add(x);
-                result.add(list);
+                ans.add(list);
             }
         }
-        return result;
+        return ans;
     }
 }
