5c1e3f6ba904683cf6c6d94213a464e8cf3dd7cb
Daisy Gao <aringao@gmail.com>
2013-10-08 23:00:40 +0800

    Search in Rotated Sorted Array II

--- leetcode/SearchinRotatedSortedArrayII/search.java
+++ leetcode/SearchinRotatedSortedArrayII/search.java
@@ -1,17 +1,16 @@
 public class Solution {
     public boolean search(int[] A, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         int l = 0, r = A.length - 1, m;
         while (l <= r) {
             m = l + (r - l >> 1);
-            if (target == A[m]) return true;
-            else if (A[l] == A[m]) l++;
+            if (A[m] == target) return true;
+            if (A[l] == A[m]) l++;
             else if (A[l] < A[m]) {
-                if (target >= A[l] && target < A[m]) r = m - 1;
+                if (A[l] <= target && target < A[m]) r = m - 1;
                 else l = m + 1;
             } else {
-                if (target > A[m] && target <= A[r]) l = m + 1;
+                if (A[m] < target && target <= A[r]) l = m + 1;
                 else r = m - 1;
             }
         }
