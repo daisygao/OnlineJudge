38a72971a2bd5b7e052bf4edff0f131a65a2bb5a
Daisy Gao <aringao@gmail.com>
2013-10-11 09:58:37 +0800

    Edit Distance

--- leetcode/EditDistance/minDistance.java
+++ leetcode/EditDistance/minDistance.java
@@ -1,19 +1,13 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int l1 = word1.length(), l2 = word2.length();
-        int sol[][] = new int[l1 + 1][l2 + 1];
-        for (int i = 0; i <= l1; i++) {
-            for (int j = 0; j <= l2; j++) {
-                if (i == 0 || j == 0) sol[i][j] = Math.max(i, j);
-                else {
-                    sol[i][j] = Math.min(sol[i - 1][j], sol[i][j - 1]) + 1;
-                    sol[i][j] = Math.min(sol[i][j], sol[i - 1][j - 1]
-					 + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1));
-                }
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int len1 = word1.length(), len2 = word2.length(), sols[][] = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0 || j == 0) sols[i][j] = Math.max(i, j);
+                else sols[i][j] = Math.min(sols[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1), Math.min(sols[i - 1][j], sols[i][j - 1]) + 1);
             }
         }
-        return sol[l1][l2];
+        return sols[len1][len2];
     }
 }
