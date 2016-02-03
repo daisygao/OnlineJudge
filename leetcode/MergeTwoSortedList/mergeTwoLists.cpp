9659090361ebfb970401aae23d77f46f4c90b2b6
Daisy Gao <aringao@gmail.com>
2016-02-03 23:02:18 +0800

    Merge Two Sorted Lists

---
+++ leetcode/MergeTwoSortedList/mergeTwoLists.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+        while (p2 >= 0) {
+            nums1[p--] = p1 >= 0 && nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
+        }
+    }
+};
