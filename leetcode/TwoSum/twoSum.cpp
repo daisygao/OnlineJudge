378bf691511a90c21ef9dd02e4121961abd487ed
Daisy Gao <aringao@gmail.com>
2017-04-06 23:02:29 +0800

    Two Sum

--- leetcode/TwoSum/twoSum.cpp
+++ leetcode/TwoSum/twoSum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        typedef unordered_map<int, vector<int>> hashmap;
-        hashmap inv;
-        vector<int> ans;
-        for (int i = 0; i < numbers.size(); i++) {
-            inv[numbers[i]].push_back(i + 1);
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, vector<int>> dict;
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++) dict[nums[i]].push_back(i);
+    for (auto v : dict) {
+      if (dict.find(target - v.first) != dict.end()) {
+        if (target - v.first != v.first) {
+          ans.push_back(v.second[0]);
+          ans.push_back(dict[target - v.first][0]);
+          break;
+        } else if (v.second.size() > 1) {
+          ans.push_back(v.second[0]);
+          ans.push_back(v.second[1]);
+          break;
         }
-        for (hashmap::iterator it = inv.begin(); it != inv.end(); it++) {
-            if (inv.find(target - it->first) != inv.end() && (it->first * 2 != target || it->second.size() > 1)) {
-                int x = it->second.back();
-                it->second.pop_back();
-                int y = inv[target - it->first].back();
-                ans.push_back(min(x, y));
-                ans.push_back(max(x, y));
-                break;
-            }
-        }
-        return ans;
+      }
     }
+    return ans;
+  }
 };
